6263610fde50dd171e30b2d03b16bfcb1ddd6a46
Reman Sunuwar <97722370+remansunuwarofficial@users.noreply.github.com>
2022-07-16 08:20:19 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+console.log("hello world");
+console.log("reman");
 
+var a = 67; //a contains 67
+console.log(a);
+a = "reman";
+console.log(a)
+let $a = 45;
+console.log($a);
+_say = "hello nepal";
+console.log(_say);
+// let 34say = "hello";
+// console.log(34say)
+// let var = "hello";
+// console.log(var);
+let name = "skillF";
+console.log(name);
